ceb71a0d256e84b8cd252e12bae0b78d22bbeda9
deoreal <radmanic@furmedia.de>
2025-09-04 22:08:34 +0200

    added index.html and other artifacts

--- main.go
+++ main.go
@@ -7,15 +7,8 @@ import (
 
 func main() {
 	mux := http.NewServeMux()
-	/*	mux.HandleFunc("/", func(w http.ResponseWriter, req *http.Request) {
-			if req.URL.Path != "/" {
-				http.NotFound(w, req)
-				return
-			}
+	mux.Handle("/", http.FileServer(http.Dir("./")))
 
-			//		fmt.Fprintf(w, "Hello world!")
-		})
-	*/
 	err := http.ListenAndServe("localhost:8080", mux)
 	if err != nil {
 		log.Fatalf("Failed to start server: %s", err)
